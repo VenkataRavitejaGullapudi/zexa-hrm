d4bd9cf398f8cc1867a3e0b9ffb2f73c9a991dd7
Raviteja <70102577+VenkataRavitejaGullapudi@users.noreply.github.com>
2021-09-18 22:27:40 +0530

    Update settings.py

--- ZexalHRM/settings.py
+++ ZexalHRM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's+ug_jth@4d-ws3s%8si)30bvx!472fzh4jurw4bp_f3-i!(l*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
